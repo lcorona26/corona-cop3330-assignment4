18f13b8d3a2255c1f2bdd653dd6170539cb10257
lcorona26 <luiscorona2602@gmail.com>
2021-07-02 12:43:38 -0400

    Intitial Update

---
+++ src/main/java/ucf/assignments/ListController.java
@@ -0,0 +1,28 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ListController {
+
+    @FXML
+    public void loadClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void saveClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void addClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void removeClicked(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void newListClicked(ActionEvent actionEvent) {
+    }
+
+}
